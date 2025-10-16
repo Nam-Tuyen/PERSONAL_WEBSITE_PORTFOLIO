cc0f981d28835f39ac3f03d80e64f2903bf111dd
Nam Tuyen <bingo.namtuyen@gmail.com>
2025-10-16 14:43:43 +0700

    UI: center-align hobby descriptions (no justify) in personal page

--- app/personal/page.tsx
+++ app/personal/page.tsx
@@ -157,7 +157,7 @@ export default function PersonalPage() {
               </div>
 
               {/* Compact 3D Carousel */}
-              <div className="max-w-4xl mx-auto relative px-3 sm:px-4 md:px-6">
+              <div className="max-w-4xl mx-auto relative">
                 {/* 3D Carousel Container */}
                 <div 
                   className="relative h-96 perspective-2000"
@@ -259,7 +259,7 @@ export default function PersonalPage() {
                               </h3>
                               
                               {/* Description */}
-                              <p className="text-gray-300 text-sm vietnamese-text leading-relaxed max-w-xs mx-auto text-justify">
+                              <p className="text-gray-300 text-sm vietnamese-text leading-relaxed max-w-xs mx-auto text-center">
                                 {hobby.description}
                               </p>
                             </div>
